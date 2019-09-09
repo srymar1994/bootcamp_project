4e2640f37f62392bcaa9736b1d01896a126eee3d
srymar1994 <srymar1994@gmail.com>
2019-09-10 00:46:16 +0200

    bar

--- project/src/components/Layouts/Bar/Bar.js
+++ project/src/components/Layouts/Bar/Bar.js
@@ -2,14 +2,37 @@ import React,{Component} from 'react';
 import "./Bar.scss";
 
 class Bar extends Component {
+
     render() {
         return (
-            <div className="bar">
+            <div className={"bar"}>
+                <div className={"box"}>
+                    <h2>Click on the shaker!</h2>
+                    <Shaker />
+                </div>
                 <div className={"bartender"}></div>
-                <div className={"list"}></div>
             </div>
         )
     }
 }
+
+
+class Shaker extends Component {
+constructor(props) {
+    super(props);
+    this.state = {
+        className: "shaker"
+    };
+    this.myRef = React.createRef();
+}
+    handleOnClick = () => {
+        this.myRef.current.classList.toggle("shake-horizontal");
+    };
+
+    render() {
+        return <div ref={this.myRef} onClick={this.handleOnClick} className={"shaker"}></div>
+    }
+}
+
 
 export default Bar;
