5369b8ea444c7d83cfb00453a60838123229df19
srymar1994 <srymar1994@gmail.com>
2019-10-24 17:06:48 +0200

    Updates

---
+++ precache-manifest.a78192c26393615502d37fe0f47fe326.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20335558480f236dddbe544dfaafde91",
+    "url": "/My-Cocktail/index.html"
+  },
+  {
+    "revision": "81863260e0a748003f25",
+    "url": "/My-Cocktail/static/css/main.9e0560e0.chunk.css"
+  },
+  {
+    "revision": "6cd1e9b693c5d8ead2f6",
+    "url": "/My-Cocktail/static/js/2.ac978f93.chunk.js"
+  },
+  {
+    "revision": "81863260e0a748003f25",
+    "url": "/My-Cocktail/static/js/main.47713904.chunk.js"
+  },
+  {
+    "revision": "a6be1f828f9ecb9f9d96",
+    "url": "/My-Cocktail/static/js/runtime~main.f7370942.js"
+  },
+  {
+    "revision": "84065a695432f4df1ad1c738e5a93978",
+    "url": "/My-Cocktail/static/media/bar-beverage-cold-248427.84065a69.jpg"
+  },
+  {
+    "revision": "caa4f155a12c849834d422a9caa286d7",
+    "url": "/My-Cocktail/static/media/barcocktail.caa4f155.png"
+  },
+  {
+    "revision": "b749ca8b32d5d58970f34794cf53a4c2",
+    "url": "/My-Cocktail/static/media/logo.b749ca8b.png"
+  },
+  {
+    "revision": "561647d67d12ddaf96923e61c68c3f31",
+    "url": "/My-Cocktail/static/media/shaker2.561647d6.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/My-Cocktail/precache-manifest.a78192c26393615502d37fe0f47fe326.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/My-Cocktail/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
